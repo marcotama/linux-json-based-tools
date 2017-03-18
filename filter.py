531473bbc92cf873e0a0c44d77aa1d327dc1052f
Marco Tamassia <tamassia.marco@gmail.com>
2017-03-18 12:46:41 +1100

    Fix bug messing with regex comparison

--- filter.py
+++ filter.py
@@ -4,6 +4,58 @@ import re
 import sys
 
 
+NUMERIC_PATTERN = re.compile(r'(\d+(?:(?:K|M|G|T|P|E)i?)?|\d+\.\d+(?:(?:K|M|G|T|P|E)i?)?)')
+
+def float_or_int(v):
+    try:
+        return int(v)
+    except ValueError:
+        try:
+            return float(v)
+        except ValueError:
+            return v
+
+def try_parse(value):
+    if value.startswith('"'):
+        return value[1:-1]
+
+    if value == 'True':
+        return True
+
+    if value == 'False':
+        return False
+
+    if re.match(NUMERIC_PATTERN, value):
+        if value.endswith('K'):
+            return float_or_int(value[:-1]) * 1e3
+        if value.endswith('M'):
+            return float_or_int(value[:-1]) * 1e6
+        if value.endswith('G'):
+            return float_or_int(value[:-1]) * 1e9
+        if value.endswith('T'):
+            return float_or_int(value[:-1]) * 1e12
+        if value.endswith('P'):
+            return float_or_int(value[:-1]) * 1e15
+        if value.endswith('E'):
+            return float_or_int(value[:-1]) * 1e18
+        if value.endswith('Ki'):
+            return float_or_int(value[:-2]) * 1024
+        if value.endswith('Mi'):
+            return float_or_int(value[:-2]) * 1024 ** 2
+        if value.endswith('Gi'):
+            return float_or_int(value[:-2]) * 1024 ** 3
+        if value.endswith('Ti'):
+            return float_or_int(value[:-2]) * 1024 ** 4
+        if value.endswith('Pi'):
+            return float_or_int(value[:-2]) * 1024 ** 5
+        if value.endswith('Ei'):
+            return float_or_int(value[:-2]) * 1024 ** 6
+
+        return float_or_int(value)
+
+    return value
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='Reads JSON lines from standard input and prints those (not) matching given conditions to standard'
@@ -61,61 +113,37 @@ if __name__ == '__main__':
                     sys.exit(1)
 
                 try:
-                    if literal.startswith('"'):
-                        pass
-                    elif literal == 'True':
-                        literal = True
-                    elif literal == 'False':
-                        literal = False
-                    elif literal.endswith('K'):
-                        literal = float(literal[:-1]) * 1e3
-                    elif literal.endswith('M'):
-                        literal = float(literal[:-1]) * 1e6
-                    elif literal.endswith('G'):
-                        literal = float(literal[:-1]) * 1e9
-                    elif literal.endswith('T'):
-                        literal = float(literal[:-1]) * 1e12
-                    elif literal.endswith('P'):
-                        literal = float(literal[:-1]) * 1e15
-                    elif literal.endswith('E'):
-                        literal = float(literal[:-1]) * 1e18
-                    elif literal.endswith('Ki'):
-                        literal = float(literal[:-2]) * 1024
-                    elif literal.endswith('Mi'):
-                        literal = float(literal[:-2]) * 1024**2
-                    elif literal.endswith('Gi'):
-                        literal = float(literal[:-2]) * 1024**3
-                    elif literal.endswith('Ti'):
-                        literal = float(literal[:-2]) * 1024**4
-                    elif literal.endswith('Pi'):
-                        literal = float(literal[:-2]) * 1024**5
-                    elif literal.endswith('Ei'):
-                        literal = float(literal[:-2]) * 1024**6
-                    else:
-                        literal = float(literal)
+                    literal = try_parse(literal)
                 except ValueError:
                     print("Literal not understood: %s" % literal, file=sys.stderr)
                     sys.exit(1)
 
+                try:
+                    prop_value = try_parse(info[prop])
+                except ValueError:
+                    print("Property not understood: %s" % info[prop], file=sys.stderr)
+                    sys.exit(1)
+
 
                 if operator == '==':
-                    matches_condition = info[prop] == literal
+                    matches_condition = prop_value == literal
                 elif operator == '>=':
-                    matches_condition = info[prop] >= literal
+                    matches_condition = prop_value >= literal
                 elif operator == '>':
-                    matches_condition = info[prop] > literal
+                    matches_condition = prop_value > literal
                 elif operator == '<=':
-                    matches_condition = info[prop] <= literal
+                    matches_condition = prop_value <= literal
                 elif operator == '<':
-                    matches_condition = info[prop] < literal
+                    matches_condition = prop_value < literal
                 elif operator == '!=':
-                    matches_condition = info[prop] != literal
+                    matches_condition = prop_value != literal
                 elif operator == '=~':
-                    matches_condition = re.match(literal, info[prop])
+                    matches_condition = bool(re.match(literal, prop_value))
                 else:
                     print("Operator not understood: %s" % operator, file=sys.stderr)
                     sys.exit(1)
 
+                # print(operator, bool(matches_condition), literal[:10] if isinstance(literal, str) else literal, prop_value[:10] if isinstance(prop_value, str) else prop_value)
                 if args.or_aggregation:
                     matches_all_conditions = matches_all_conditions or matches_condition
                 else:
@@ -124,6 +152,8 @@ if __name__ == '__main__':
             else:
                 print("Condition not understood: %s" % condition, file=sys.stderr)
                 sys.exit(1)
-
+        # print(matches_all_conditions)
+        # print()
+        #
         if matches_all_conditions != args.reverse:
             print(line, end='')
