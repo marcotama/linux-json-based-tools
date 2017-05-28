9bb59e0b7e03c2faf1e81400cc56598e2c410b2a
Marco Tamassia <tamassia.marco@gmail.com>
2017-05-28 13:56:58 +0900

    Improve regex

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import re
 
 print(re.match(r'([a-zA-Z0-9-_]+)', 'is-directory'))
 print(re.match(r'(==|>=|>|<=|<|!=|=~)', '=='))
-print(re.match(r'\d+(?:(?:K|M|G|T|P|E)?i?)|\d+\.\d+(?:(?:K|M|G|T|P|E)?i?)|\d+\.\d+e\d+|"[^"]+"|True|False', '1'))
+print(re.match(r'\d+(?:(?:[KMGTPE])?i?)|\d+\.\d+(?:(?:[KMGTPE])?i?)|\d+\.\d+e\d+|"[^"]+"|True|False', '1'))
